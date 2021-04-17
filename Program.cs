0807a4106d7e214ba8e932ab2ce4de0d4f1fa42c
Саня <sanya@Sergejs-MacBook-Air.local>
2021-04-17 19:17:15 +0600

    Объявлены переменные для количества однозначных цифр(countSingleDigit), двухзначных чисел(countTwoDigit), трехзначных чисел(countThreeDigit), переменная для ввода с клавы,  массив и размер массива

--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace Task8v5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //* 1. Дано число n. Определить сколько среди них 
+            //*однозначных, двухзначных, трехзначных.
+
+            string input;
+            int size;
+            Console.Write("Введите размер массива: ");
+            input = Console.ReadLine();
+            size = int.Parse(input);
+            int[] numbersArray = new int[size];
+            int countSingleDigit = 0;
+            int countTwoDigit = 0;
+            int countThreeDigit = 0;
+
         }
     }
 }
