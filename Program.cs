b90ca761059c26646147165c5775b40bc22ad19c
Саня <sanya@Sergejs-MacBook-Air.local>
2021-04-17 19:26:06 +0600

    Добавлен цикл для ввода значений в массив, создан второй цикл for, который проходит по элементам массива и в котором есть цикл while(tmp >= 1), который уменшает tmp в 10 раз и считает количество цифр, перед циклом while переменной tmp присватвается значение элемента массива

--- Program.cs
+++ Program.cs
@@ -18,6 +18,45 @@ namespace Task8v5
             int countSingleDigit = 0;
             int countTwoDigit = 0;
             int countThreeDigit = 0;
+            int countDigit = 0;
+
+            for (int i = 0; i < numbersArray.Length; i++)
+            {
+                Console.Write($"Введите {i + 1} число массива: ");
+                input = Console.ReadLine();
+                numbersArray[i] = int.Parse(input);
+            }
+
+            int tmp;
+            for (int i = 0; i < numbersArray.Length; i++)
+            {
+                tmp = numbersArray[i];
+                while(tmp >= 1)
+                {
+                    tmp /= 10;
+                    countDigit++;
+                }
+                
+                switch(countDigit)
+                {
+                    case 1:
+                    {
+                        countSingleDigit++;
+                        break;
+                    }
+                    case 2:
+                    {
+                        countTwoDigit++;
+                        break;
+                    }
+                    case 3:
+                    {
+                        countThreeDigit++;
+                        break;
+                    }
+                }
+                countDigit = 0;
+            }
 
         }
     }
